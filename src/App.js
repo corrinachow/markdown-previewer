02c5b8153c4c36b8bcc522fe0fd7b383ec766d04
corrinachow <corrina.yy.chow@gmail.com>
2019-02-10 13:06:09 -0500

    Add carriage return <br> functionality and opening links in new tab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
+const renderer = new window.marked.Renderer();
+
+renderer.link = (href, title, text) =>
+  `<a target="_blank" href="${href}">${text}` + "</a>";
 
 const MarkdownPreviewer = () => {
-  const defaultText = `# Hello, World!`;
-
   const [text, setText] = useState(defaultText);
 
   const handleInputChange = event => {
@@ -11,10 +14,62 @@ const MarkdownPreviewer = () => {
 
   return (
     <>
-      <textarea id="editor" onChange={handleInputChange} />
-      <div id="preview">{text}</div>
+      <textarea id="editor" onChange={handleInputChange}>
+        {text}
+      </textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: window.marked(text, { renderer: renderer })
+        }}
+      />
     </>
   );
 };
+
+const defaultText = `# Welcome to my React Markdown Previewer!
+## This is a sub-heading
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.corrinachow.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)`;
 
 export default MarkdownPreviewer;
